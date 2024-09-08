cfca6f1931a1ba7b65ed24c843b1f49f6846c70b
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2024-09-08 09:22:14 +0600

    Add files via upload

---
+++ L3_T1/SESSIONAL/CSE_318_DATA_COM/class_04/checksum.m
@@ -0,0 +1,30 @@
+clc;
+clear;
+close all;
+
+data = [7 11 12 0 6];
+
+len = length(data);
+cs = sum(data);
+cs = dec2bin(cs);
+
+
+len = length(cs);
+mainBin = cs(len-4+1:len)
+wrapBin = cs(1:len-4)
+
+mainDec = bin2dec(cs(len-4+1:len))
+wrapDec = bin2dec(cs(1:len-4))
+wrappedsum = (dec2bin(mainDec+wrapDec), 4))
+
+checksum = "";
+for i=1:1:4
+    if(wrappedsum(i)) == '1')
+        checksum = checksum+"0";
+    else
+        checksum = checksum+"1";
+    end
+
+end
+
+checksum = bin2dec(checksum)
